e27febd075cf0e9b9b6e6ef27c6068041caf54f5
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2025-09-15 21:03:31 +0200

    solution workshop-3 2025

--- src/main/java/fr/soat/banking/domain/TransferProcessManager.java
+++ src/main/java/fr/soat/banking/domain/TransferProcessManager.java
@@ -34,12 +34,12 @@ public class TransferProcessManager  {
     @EventListener
     public void on(FundCredited fundCredited) {
         log.info("consuming {}", fundCredited.getClass().getSimpleName());
-        //FIXME
-        // when triggered on a FundCredited
         // 1. reload the sender Account
+        Account account = accountRepository.load(fundCredited.getSenderAccountId());
         // 2. make debit() decision on sender account
+        account.debit(fundCredited.getAccountId(), fundCredited.getAmount());
         // 3. save the sender Account
-        throw new RuntimeException("implement me !");
+        accountRepository.save(account);
     }
 
 }
